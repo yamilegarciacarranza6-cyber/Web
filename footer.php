08d31cc7612af6be0be2c62f0d4572c18ab7fed0
Yamile <yamilegarcia0420@gmail.com>
2025-09-28 13:41:51 -0600

    Actualizo footer y agrego formularioEditar_producto

--- footer.php
+++ footer.php
@@ -5,15 +5,11 @@
             <img src="https://www.w3.org/Icons/valid-html401"
                 alt="¡HTML Válido!" style="border:0; width:88px; height:31px;">
         </a>
-    </p>
-
-    <p style="text-align:center; margin-top:20px;">
         <a href="https://jigsaw.w3.org/css-validator/check/referer">
             <img style="border:0;width:88px;height:31px"
                 src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
                 alt="¡CSS Válido!" />
         </a>
     </p>
-
 </footer>
 
